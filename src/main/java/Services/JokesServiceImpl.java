476707318315a104a88dc4aad15577388cc4925d
sallauddin023 <skallauddin023@gmail.com>
2020-10-05 17:03:33 +0530

    Initial Commit

--- src/main/java/Services/JokesServiceImpl.java
+++ src/main/java/Services/JokesServiceImpl.java
@@ -1,4 +1,23 @@
 package Services;
 
-public class JokesServiceImpl {
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		super();
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
+	}
+
+
+
+	@Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
 }
